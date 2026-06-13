3f65fb7829fef5edb344dab1d2484a59384af05d
AmanVatsSharma <aman95026@gmail.com>
2026-06-13 11:10:20 +0530

    feat: add warehouses domain library with initial setup and configuration

---
+++ libs/domains/warehouses/src/index.ts
@@ -0,0 +1,7 @@
+export * from './lib/warehouses.module';
+export * from './lib/warehouses.service';
+export * from './lib/warehouses.resolver';
+export * from './lib/warehouse.model';
+export * from './lib/dto/create-warehouse.input';
+export * from './lib/dto/update-warehouse.input';
+export * from './lib/dto/upsert-warehouse-coverage.input';
