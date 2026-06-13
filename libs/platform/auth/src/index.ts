4a3eae6069802a1e79d0da3b975d2fb9b10ae52a
AmanVatsSharma <aman95026@gmail.com>
2026-06-13 11:10:42 +0530

    feat: update auth library configuration and exports for improved structure

--- libs/platform/auth/src/index.ts
+++ libs/platform/auth/src/index.ts
@@ -1,1 +1,6 @@
-export * from './lib/auth';
+export * from './lib/auth.service';
+export * from './lib/auth.module';
+export * from './lib/jwt.strategy';
+export * from './lib/auth.guards';
+export * from './lib/roles.decorator';
+export * from './lib/current-user.decorator';
